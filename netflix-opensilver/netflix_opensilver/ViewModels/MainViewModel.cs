e36c4c2470243a4297f9967d7a0e2ce81b1cf43c
tmdgjs9525 <56830330+tmdgjs9525@users.noreply.github.com>
2025-02-25 23:35:19 +0900

    observableproperty에 property를 사용할 때에는 public

--- netflix-opensilver/netflix_opensilver/ViewModels/MainViewModel.cs
+++ netflix-opensilver/netflix_opensilver/ViewModels/MainViewModel.cs
@@ -12,15 +12,18 @@ namespace netflix_opensilver.ViewModels
     internal partial class MainViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private partial MediaInfo RecommendedItem { get; set; }
+        public partial MediaInfo RecommendedItem { get; set; }
 
+        [ObservableProperty]
+        private MediaInfo test;
         public MainViewModel()
         {
             RecommendedItem = new MediaInfo
             {
-                PosterUrl = "https://occ-0-4960-993.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABfbig2zd031EliCvJbjxKH9daeu9IgJYQEVbbVcXjGpMS990_Y6QsqerZdCZQiTgqTBNDHq0BloY9G-R8Q-sjHrPHI4ObuAGXsRN.webp?r=ad0",
-                Description = "가나다라마바사아카차자파타라 나라다라아라마 바아라다라아나라달아ㅏ",
+                PosterUrl = "https://occ-0-4960-993.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABTtqEr23wkU_fY69qASaHlwsopBiJnEWX0kZJs1SPUljgU7dXT_wj_RUm9gTSbwhXB4wNcNm7ZYteEIxssmgXWQjIZC8qOvlAXXT.webp?r=6e4",
+                Description = "asdasa",
             };
+
         }
     }
 }
