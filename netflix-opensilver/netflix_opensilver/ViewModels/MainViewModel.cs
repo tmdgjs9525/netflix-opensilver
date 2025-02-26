e73d97882b2b76f2fe6ad40fe8fe156db4892ccc
tmdgjs9525 <56830330+tmdgjs9525@users.noreply.github.com>
2025-02-26 22:06:01 +0900

    추천리스트 샘플

--- netflix-opensilver/netflix_opensilver/ViewModels/MainViewModel.cs
+++ netflix-opensilver/netflix_opensilver/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,10 +17,11 @@ namespace netflix_opensilver.ViewModels
         public partial MediaInfo RecommendedItem { get; set; }
 
         [ObservableProperty]
-        private MediaInfo test;
+        public partial ObservableCollection<RecommendationList> RecommendationList { get; set; }
 
         [ObservableProperty]
-        public partial ObservableCollection<RecommendationList> RecommendationList { get; set; }
+        public partial RecommendationList Test { get; set; }
+
 
         public MainViewModel()
         {
@@ -27,6 +29,29 @@ namespace netflix_opensilver.ViewModels
             {
                 PosterUrl = "https://occ-0-4960-993.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABTtqEr23wkU_fY69qASaHlwsopBiJnEWX0kZJs1SPUljgU7dXT_wj_RUm9gTSbwhXB4wNcNm7ZYteEIxssmgXWQjIZC8qOvlAXXT.webp?r=6e4",
                 Description = "asdasa",
+            };
+
+            Test = new RecommendationList
+            {
+                RecommendationListName = "User를 위한 콘텐츠",
+                RecommendList = new ObservableCollection<MediaInfo>
+                {
+                    new MediaInfo
+                    {
+                        PosterUrl = "https://occ-0-4960-993.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRL5uUKHQnSZM6m9QBnI3eMCkB3GhhARVRFX4O_vSGv0ZC7DBYkMh3Yr_cXp75K0XYJ36Xmv9ai9OGhOz-NoDY4jQNrqB6N0ljHHdBJ2zxyaDF68qhF00e1OaSG8x4qAcPgORKUMH9NzkqCIjJr2-TJcuRNFHHCGi7A8Ropn-FMN7TfBJnsd_JRmwyX5MpntmaeWw1ZH3o_m4C7mhcZ-CppO6uUNgBIdj5ApY2Fyso5A8Vs6AsoDokmcfWugeL-fVRCdgyDtIGTQiCzZMw3aZiWDDGNAlp1RKL2dG6LkWd5ZCKoFa-hJDa7l1MdzO0Polptt4ekjA3NakbpeXWFWdw.webp?r=ef0",
+
+                    },
+                    new MediaInfo
+                    {
+                        PosterUrl = "https://occ-0-4960-993.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRL5uUKHQnSZM6m9QBnI3eMCkB3GhhARVRFX4O_vSGv0ZC7DBYkMh3Yr_cXp75K0XYJ36Xmv9ai9OGhOz-NoDY4jQNrqB6N0ljHHdBJ2zxyaDF68qhF00e1OaSG8x4qAcPgORKUMH9NzkqCIjJr2-TJcuRNFHHCGi7A8Ropn-FMN7TfBJnsd_JRmwyX5MpntmaeWw1ZH3o_m4C7mhcZ-CppO6uUNgBIdj5ApY2Fyso5A8Vs6AsoDokmcfWugeL-fVRCdgyDtIGTQiCzZMw3aZiWDDGNAlp1RKL2dG6LkWd5ZCKoFa-hJDa7l1MdzO0Polptt4ekjA3NakbpeXWFWdw.webp?r=ef0",
+
+                    },
+                    new MediaInfo
+                    {
+                        PosterUrl = "https://occ-0-4960-993.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRL5uUKHQnSZM6m9QBnI3eMCkB3GhhARVRFX4O_vSGv0ZC7DBYkMh3Yr_cXp75K0XYJ36Xmv9ai9OGhOz-NoDY4jQNrqB6N0ljHHdBJ2zxyaDF68qhF00e1OaSG8x4qAcPgORKUMH9NzkqCIjJr2-TJcuRNFHHCGi7A8Ropn-FMN7TfBJnsd_JRmwyX5MpntmaeWw1ZH3o_m4C7mhcZ-CppO6uUNgBIdj5ApY2Fyso5A8Vs6AsoDokmcfWugeL-fVRCdgyDtIGTQiCzZMw3aZiWDDGNAlp1RKL2dG6LkWd5ZCKoFa-hJDa7l1MdzO0Polptt4ekjA3NakbpeXWFWdw.webp?r=ef0",
+
+                    }
+                }
             };
 
         }
